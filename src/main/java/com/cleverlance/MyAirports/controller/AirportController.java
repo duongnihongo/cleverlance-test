ea19c0500e467041ad9248356b88546009ecec35
duongdt <duongdt@five9.vn>
2019-09-30 17:52:22 +0700

    add description for APIs

--- src/main/java/com/cleverlance/MyAirports/controller/AirportController.java
+++ src/main/java/com/cleverlance/MyAirports/controller/AirportController.java
@@ -2,6 +2,8 @@ package com.cleverlance.MyAirports.controller;
 
 import com.cleverlance.MyAirports.DTO.ResponseObject;
 import com.cleverlance.MyAirports.service.AirportService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,15 +12,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/airport/*")
+@Api(value = "AIRPORT MANAGEMENT")
 public class AirportController {
     @Autowired
     AirportService airportService;
 
+    @ApiOperation(value = "Get airport data from Airlabs.co and save it to Database")
     @GetMapping(value = "/myAirports")
     public ResponseObject myAirports(@RequestParam(name = "apiKey") String apiKey) {
         return airportService.myAirports(apiKey);
     }
 
+    @ApiOperation(value = "Get specific airport by its code")
     @GetMapping(value = "/getAirportByCode")
     public ResponseObject getAirportByCode(@RequestParam(name = "code") String code) {
         return airportService.getAirportByCode(code);
--- src/main/java/com/cleverlance/MyAirports/controller/PingController.java
+++ src/main/java/com/cleverlance/MyAirports/controller/PingController.java
@@ -2,6 +2,8 @@ package com.cleverlance.MyAirports.controller;
 
 import com.cleverlance.MyAirports.DTO.ResponseObject;
 import com.cleverlance.MyAirports.service.PingService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,10 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/ping/*")
+@Api(value = "PING SYSTEM")
 public class PingController {
     @Autowired
     PingService pingService;
 
+    @ApiOperation(value = "Get all airports from Database and print out them to console")
     @GetMapping(value = "/ping")
     public ResponseObject printOutAllAirports() {
         return pingService.printOutAllAirports();
